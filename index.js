2cd7d9443473aa4be0f321eaa7013b6e110ed551
Lokesh Gurung <lokeshgurungwj@gmail.com>
2022-01-27 20:22:29 +0545

    Connect to the local DB

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Connect to the local MongoDB database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Namaste');
